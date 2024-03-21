7668966f0fe4f4bae4084867d8c59036edddb5da
KYKong97 <124419374+KYKong97@users.noreply.github.com>
2024-03-21 15:45:14 +0800

    Update ms_deform_attn_func.py

--- mask2former/modeling/pixel_decoder/ops/functions/ms_deform_attn_func.py
+++ mask2former/modeling/pixel_decoder/ops/functions/ms_deform_attn_func.py
@@ -26,7 +26,8 @@ except ModuleNotFoundError as e:
         "\t`cd mask2former/modeling/pixel_decoder/ops`\n"
         "\t`sh make.sh`\n"
     )
-    raise ModuleNotFoundError(info_string)
+    print(info_string)
+    # raise ModuleNotFoundError(info_string)
 
 
 class MSDeformAttnFunction(Function):
